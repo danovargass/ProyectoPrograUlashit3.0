544880ad7f8b9d9a5fa35e3e6b7e98976d971e33
danovargass <danovargass@users.noreply.github.com>
2017-08-01 23:36:38 -0600

    Comienzo integracion
    Ahorita hay 3 pantallas, que estan siendo relativamente funcionales, digame que le parece la vara para ver si sigo montandome con eso. Aca le dejo unas varas que defini como para mantener concistente la aplicacion:
    
    Main frame sizes
    870x565
    Boton standard
    120px wide
    Renderizar factura a 270px width (esto ya lo cambie)
    
    Fonts Proyecto final
    H1: Silom 18 Italic

--- Proyecto_Final_Progra/src/proyecto_final_progra/Cajero.java
+++ Proyecto_Final_Progra/src/proyecto_final_progra/Cajero.java
@@ -7,20 +7,54 @@ public class Cajero {
     
     private String nombre;
     private int id;
+
+    public Cajero(String nombre, int id) {
+        this.nombre = nombre;
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    /////////////
     
+    //este metodo podria no estar y solo dejarse con los constructores y gets y sets
+    
+    ////////////
     public void login(){ //Funcion de Log-in, tiene un for para poder repetir el ingreso en caso erroneo de contrasena
-    	for (int i=0; i==0 ;i++){
-            System.out.println("Ingreso");
-    		nombre =JOptionPane.showInputDialog("Ingrese su nombre");
-        	id = Integer.parseInt(JOptionPane.showInputDialog("Ingrese su id"));
-            if (id == 1155 && nombre.contains("Juan")){
-            		JOptionPane.showMessageDialog(null, "Su ingreso ha sido correcto");
-            	} else{
-            		JOptionPane.showMessageDialog(null, "Ingreso erroneo");
-            		i = i-1;
-            	}
+    	try {
+            for (int i = 0; i == 0; i++) {
+                
+                //System.out.println("Ingreso");
+                //nombre = JOptionPane.showInputDialog("Ingrese su nombre");
+                //id = Integer.parseInt(JOptionPane.showInputDialog("Ingrese su id"));
+                if (id == 1155 && nombre.contains("Juan")) {
+                    JOptionPane.showMessageDialog(null, "Su ingreso ha sido correcto");
+                } else {
+                    JOptionPane.showMessageDialog(null, "Ingreso erroneo");
+                    i = i - 1;
+                }
             }
-    	}
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Out of bounds");
+        }catch (NumberFormatException e) {
+            System.out.println("Por favor intente de nuevo");
+        }
+    }
+    
+    
     	
     
     public void logout(){
